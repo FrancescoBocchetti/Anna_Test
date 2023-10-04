e416981927eea18fe57ac763f5ffb894b73edc65
BOCCHETTI Francesco <BOCCHETTI@bts.leo>
2023-10-04 09:10:50 +0200

    Modif index.html bob

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function showTime(){
     var h = date.getHours(); 
     var m = date.getMinutes(); 
     var s = date.getSeconds(); 
-    
+    var time = h + ":" + m + ":" + s;
     h = (h < 10) ? "0" + h : h;
     m = (m < 10) ? "0" + m : m;
     s = (s < 10) ? "0" + s : s;
